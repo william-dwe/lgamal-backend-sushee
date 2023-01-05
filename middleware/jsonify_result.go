f1494976aa13669ed18aeb78c5129870d92dbb6b
@william.ciptono <william.ciptono@shopee.com>
2023-01-05 13:34:09 +0700

    fix: change CORS access controll allow origin

--- middleware/jsonify_result.go
+++ middleware/jsonify_result.go
@@ -9,7 +9,7 @@ import (
 func JSONifyResult() gin.HandlerFunc {
 	return func(c *gin.Context) {
         c.Writer.Header().Set("Content-Type", "application/json")
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "https://lgamal-frontend-sushee.vercel.app/")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "https://lgamal-frontend-sushee.vercel.app")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
